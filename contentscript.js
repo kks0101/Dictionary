5941f46d01e6bf427540cd9b704d306178dbd3e2
Krishna Kant Singh <kks1102000@gmail.com>
2020-01-15 01:05:16 +0530

    added css for the modal

--- contentscript.js
+++ contentscript.js
@@ -1,41 +1,26 @@
- 
-console.log("content script");
-/*var iframe = document.createElement('iframe');
-iframe.src = chrome.extension.getURL("modal.html");
-//iframe.src = "https://google.co.in"
-iframe.frameBorder = 0;
-iframe.id = "myFrame";
-iframe.height = "400";
-$(iframe).show();
-$(iframe).appendTo("body");
-*/
+
 var div1 = document.createElement('div');
 div1.setAttribute('id', 'myModal');
 div1.setAttribute('class', 'modal');
-var div2 = document.createElement('div');
-div2.setAttribute('class', 'modal-content');
-/*var span = document.createElement('span');
-span.setAttribute('class', 'close');
-span.setAttribute*/
-var content = document.createElement('p');
-content.setAttribute('id', 'displayText')
-content.innerHTML = "hello there!";
-div2.appendChild(content);
-div1.appendChild(div2);
+
+div1.innerHTML = '<div class="modal-content"><div class="modal-header"><h2>Word</h2></div><div class="modal-body"><p>Meaning</p></div></div>';
+
+
 document.body.appendChild(div1);
+
 var modal = document.getElementById("myModal");
 window.onclick = function(event){
 	if(event.target == modal){
 		modal.style.display = "none";
 	}
 }
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
             switch (request.type){
                 case "openModal":
                 	
-                    console.log(request.message);
-                    
-                    
+                    //console.log(request.message);
+                                        
                     var xhr = new XMLHttpRequest();
 					xhr.open('GET', "https://dictionaryapi.com/api/v3/references/collegiate/json/"+request.message+"?key=e6aacbf3-b030-407f-9670-5bcf7f2f57e0");
 					xhr.setRequestHeader("Accept", "application/json");
@@ -50,6 +35,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 							x = JSON.parse(response);
 							//console.log(x);
 							var displayMeaning = document.querySelector('.modal-content p');
+							var displayWord = document.querySelector('.modal-header h2');
+							displayWord.innerHTML = request.message;
 							displayMeaning.innerHTML = x[0]["shortdef"];
 						
 						}
